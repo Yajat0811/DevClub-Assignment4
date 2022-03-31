e6fa83dc7907d7be60e0c360b3446a9117eccd10
Yajat0811 <97752332+Yajat0811@users.noreply.github.com>
2022-03-31 22:08:50 +0530

    Update index.js

--- Wordle+/index.js
+++ Wordle+/index.js
@@ -1,13 +1,25 @@
 const http = require("http");
 
-const SECRET = "CIGAR"; // You can set any word as the secret answer
+
 
 function myFunction(req, res) {
 	// console.log({req}); // You can uncomment this to see the request object
 	console.log(req.url);
 
 	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
+	const SECRET = "CIGAR"; // You can set any word as the secret answer
+const Guess =req.url.split("Q")[1]
+let answer="response"
+for(let i=0,i<5,i++);
+	if(Guess[i]==SECRET[i]){
+		answer=answer+"g"
+	}
+        else(Guess[i]!=Secret[i]){
+		if(Guess[i] not in SECRET)
+			answer=answer+"b"
+		else
+			answer+="y"
+	const feedback = "answer"; // Write logic to compare the word with the secret, and generate the feedback string
 
 	res.write(feedback);
 	res.end();
